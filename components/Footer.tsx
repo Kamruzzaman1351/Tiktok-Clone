600b46a71671e14af1c363040b62ac25125d86af
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-07-19 23:40:28 +0600

    Discover and Footer done and adding the utility for the app

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { NextPage } from 'next'
 
-const Footer = () => {
+import { footerList1, footerList2, footerList3 } from '../utils/constants'
+import Link from 'next/link'
+const List = ({items, mt}: {items:string[], mt:boolean}) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {items.map(item => (
+      <p key={item} 
+        className="text-gray-400 text-sm hover:underline cursor-pointer">
+        {item}</p>
+    ))}
+  </div>
+)
+
+
+const Footer: NextPage = () => {
+  const currentYear = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block'>
+      <List items={footerList1} mt={false}/>
+      <List items={footerList2} mt />
+      <List items={footerList3} mt />
+      <div className='text-gray-400 text-sm mt-5'>
+        {`© ${currentYear} TikTok`}
+        <h3>Created By: <Link href="https://github.com/Kamruzzaman1351" target="_blank" >
+          <span className="text-bold text-[#F51997] cursor-pointer">Kamruzzaman</span></Link>
+        </h3>
+      </div>
+    </div>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,9 +10,9 @@ import { SiTiktok } from "react-icons/si"
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-4 px-4'>
       <Link href="/" >
-        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+        <div className='cursor-pointer w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
           <h1 className='text-red-600 text-3xl inline-flex justify-between gap-2'>
             <SiTiktok /> TikTok
           </h1>
