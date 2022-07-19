cbee26af4a7ab87ffb98c1425674ad7624d21b90
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-07-19 17:47:17 +0600

    Installing tailwind css and other packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>TikTok App</title>
         <meta name="description" content="TikTok App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h2>TikTok APP</h2>
+      <main>
+        <h2 className="text-3xl text-center my-3 mx-auto text-blue-600">TikTok APP</h2>
       </main>
 
     </div>
