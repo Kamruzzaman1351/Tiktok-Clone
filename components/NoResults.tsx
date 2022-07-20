a7ef9a20841cda7790aeadb204b5aad26e7de848
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-07-20 21:24:17 +0600

    Setup the videocard and No result page

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import { MdOutlineVideocamOff } from 'react-icons/md'
 interface IText {
   text: string;
 }
 
 const NoResults = ({text}: IText) => {
   return (
-    <div>NoResults</div>
+    <div className='flex flex-col justify-center items-center h-full w-full'>
+      <p className='text-8xl'>
+        <MdOutlineVideocamOff />
+      </p>
+      <p className='text-2xl text-center'>{text}</p>
+    </div>
   )
 }
 
